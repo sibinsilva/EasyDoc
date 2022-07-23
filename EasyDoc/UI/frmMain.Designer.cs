1a97ff8ab67c1e79d7e8e3f159813daf9f7a6f86
sibinsilva <sibinsilva@gmail.com>
2022-07-23 09:30:01 +0100

    Prevent UI maximize button

--- EasyDoc/UI/frmMain.Designer.cs
+++ EasyDoc/UI/frmMain.Designer.cs
@@ -369,6 +369,7 @@
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "EasyDoc";
